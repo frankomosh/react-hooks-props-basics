531be4ec5899d7c58b919160626008b8863fb68b
Frank-197 <frankomosh197@gmail.com>
2022-07-24 16:32:48 +0300

    Done

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 function Comment(props) {
+  console.log(props);
   return <div className="comment">{props.commentText}</div>;
 }
 
-export default Comment;
+export default Comment; 
